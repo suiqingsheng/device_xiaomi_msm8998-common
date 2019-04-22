f770497b0925ec6a764feaab546543a5928a46f2
arian <arian.kulmer@web.de>
2019-04-22 22:11:06 +0200

    Revert "msm8998-common: Drop libbt-vendor"
    This reverts commit 0618ffbbcd142b7c10b114d52546417d59629239.

--- msm8998.mk
+++ msm8998.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8998 \
