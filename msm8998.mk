f62c1fa4d1181a475372b68626a7974b82ab7cc2
arian <arian.kulmer@web.de>
2019-04-22 22:11:15 +0200

    Revert "msm8998-common: switch to QCOM HIDL bluetooth stack"
    This reverts commit d20e887052de283fb97b2416bf567473b1563326.

--- msm8998.mk
+++ msm8998.mk
@@ -127,7 +127,8 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
+    com.dsi.ant.antradio_library \
+    libantradio
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
@@ -143,6 +144,9 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
