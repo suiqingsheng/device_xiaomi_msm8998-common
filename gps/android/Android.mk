5bd3049b3a023b9192c0a2d43da249c49da312c4
arian <arian.kulmer@web.de>
2019-04-26 22:46:36 +0200

    msm8998-common: gps: Import from LA.UM.7.4.r1-05000-8x98.0
    Apply our patches again:
    
    commit e06cbe6e18624479b344cd1d0373127676bd9ded
    Author: Volodymyr Zhdanov <wight554@gmail.com>
    Date:   Thu Mar 21 21:59:38 2019 +0000
    
        msm8998-common: gps: fix build
    
    commit b70912270935d6922ea7df3173320068001f4c8f
    Author: Thierry Strudel <tstrudel@google.com>
    Date:   Tue Apr 25 14:27:11 2017 -0700
    
        msm8998-common: fix android.hardware.gnss@1.0-service-qti
    
        Make service part of 'hal' class.
        Install files in vendor partition.
        Register instance as 'default' as expected by framework.
    
        Change-Id: I487fad4df872397ab76cea377e5304c956a756da
        Signed-off-by: Thierry Strudel <tstrudel@google.com>
    
    commit 56aa86d878a92ad9b21c8edb17aac237f0739729
    Author: Jiyong Park <jiyong@google.com>
    Date:   Wed Sep 6 12:10:07 2017 +0900
    
        msm8998-common: Don't rely on transitively included headers
    
        One must explicitly include what it need.
        time.h for struct tm.
        sys/time.h for gettimeofday, etc.
    
        Bug: 37629934
        Test: build
    
        Merged-In: Iacd5317d958f61df062fca40fbf2e8992c7fbb44
        Change-Id: I19e1043f23dbe85836b73714a4f1440159ebe419
    
    commit 75adb384e2ef3f8ce485849305f4411d68e38d5d
    Author: Steven Moreland <smoreland@google.com>
    Date:   Thu Jun 29 17:13:51 2017 -0700
    
        msm8998-common: Build libgps.utils with VNDK.
    
        Test: m -j BOARD_VNDK_VERSION=current libgps.utils
        Bug: 37342627
        Change-Id: Ifcd9344245cf689d4075d34b89faa689647b8b0b
    
    Change-Id: If4670ba76f142dfcf7a0a3196e2206b1b80aa4a8

--- gps/android/Android.mk
+++ gps/android/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.gnss@1.0-impl-qti
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_OWNER := qti
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := \
@@ -37,6 +36,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     libhidltransport \
     libhwbinder \
+    libcutils \
     libutils \
     android.hardware.gnss@1.0 \
 
@@ -44,7 +44,6 @@ LOCAL_SHARED_LIBRARIES += \
     libloc_core \
     libgps.utils \
     libdl \
-    libloc_pla \
     liblocation_api \
 
 LOCAL_CFLAGS += $(GNSS_CFLAGS)
@@ -60,10 +59,10 @@ endif # BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
 ifeq ($(BUILD_GNSS_HIDL_SERVICE), true)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.gnss@1.0-service-qti
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_INIT_RC := android.hardware.gnss@1.0-service-qti.rc
-LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := \
     service.cpp \
 
@@ -74,6 +73,7 @@ LOCAL_HEADER_LIBRARIES := \
     libloc_core_headers \
     libloc_pla_headers \
     liblocation_api_headers
+
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
