2268401b91f21b53496023a828853b0f591d1b56
arian <arian.kulmer@web.de>
2019-04-27 11:35:05 +0200

    msm8998-common: Build new GPS HAL
    * nuke the BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE flag to not trigger potentially cloned other GPS HALs
    
    Change-Id: I358e41ade8dca1a84538bc8d17bb4a1abb21b82e

--- gps/pla/Android.mk
+++ gps/pla/Android.mk
@@ -16,7 +16,6 @@ GNSS_CFLAGS := \
     -Wno-error=switch \
     -Wno-error=date-time
 
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 ifneq ($(BUILD_TINY_ANDROID),true)
 
 LOCAL_PATH := $(call my-dir)
@@ -27,4 +26,3 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/android
 include $(BUILD_HEADER_LIBRARY)
 
 endif # not BUILD_TINY_ANDROID
-endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
